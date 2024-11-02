c71105aaff03fd8be414fc29dece0000ae5e209f
Rohan sahu <therohansahu7@gmail.com>
2024-11-03 01:49:23 +0530

    allowing all origins

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,21 +3,14 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'https://pro-management-one.vercel.app'];
+
 
 
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if( allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-}))
+    origin: "*",
+    credentials: true
+}));
 
 app.use(express.json({ limit: "16kb"}))
 app.use(express.urlencoded({ extended:true, limit: "16kb"}))
