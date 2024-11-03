07ce42c6f2538337c1e3fdb67506d11c2087388b
Rohan sahu <therohansahu7@gmail.com>
2024-11-03 11:59:30 +0530

    Cors policy modification

--- Server/src/app.js
+++ Server/src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: function (origin, callback) {
       
       if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        const msg = 'The CORSpolicy for this site does not allow access from the specified Origin.';
         return callback(new Error(msg), false);
       }
       return callback(null, true);
@@ -24,7 +24,7 @@ app.use(cors({
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
   }));
-  
+
 
 app.use(express.json({ limit: "16kb"}))
 app.use(express.urlencoded({ extended:true, limit: "16kb"}))
